6ccd2c7702dfcccc475415a36994d12b2d0ed982
Rodolfo Cordero <rcor.cr@gmail.com>
2023-01-25 00:16:33 -0300

    migration ready

--- migration/src/__init__.py
+++ migration/src/__init__.py
@@ -1,12 +1,18 @@
 from yoyo import read_migrations, get_backend
 import os
 
-if __name__ == '__init__':
+if __name__ == '__main__':
     user=os.environ['USER']
     password=os.environ['PASSWORD']
     database=os.environ['DATABASE']
     host=os.environ['HOST']
-    backend = get_backend(f'postgres://{user}:{password}@{host}/{database}')
+    db_connection='postgres://{user}:{password}@{host}/{database}'.format(user=user,password=password,host=host,database=database)
+    print(user)
+    print(host)
+    print(db_connection)
+    backend = get_backend(db_connection)
     migrations = read_migrations('./migrations')
+    print("run migrations")
     backend.apply_migrations(backend.to_apply(migrations))
+    print("end migrations")
     
--- migration/src/migrations/0000001.create_tables.py
+++ migration/src/migrations/0000001.create_tables.py
@@ -1,11 +1,25 @@
 from yoyo import step
-
+import os
 __depends__ = {}
 
 steps = [
-    step("CREATE TABLE metadata (metadata_id VARCHAR(20), org_name VARCHAR(20), file_name VARCHAR(20), date_begin timestamp, date_end timestamp, PRIMARY KEY (id))",
+    step("CREATE TABLE metadata (metadata_id VARCHAR(20), org_name VARCHAR(20), file_name VARCHAR(20), date_begin timestamp, date_end timestamp, PRIMARY KEY (metadata_id))",
          "DROP TABLE metadata"),
-    step("CREATE TABLE reports (report_id serial,metadata_id VARCHAR(20) NOT NULL,, FOREIGN KEY (metadata_id) REFERENCES metadata (metadata_id) ) ","DROP TABLE reports"),
+    step("""CREATE TABLE reports (
+                report_id serial,metadata_id VARCHAR(20) NOT NULL,
+                source_ip VARCHAR(20), 
+                _count NUMERIC, 
+                policy_evaluated_disposition VARCHAR(20),
+                policy_evaluated_dkim VARCHAR(20),
+                policy_evaluated_spf VARCHAR(20),
+                identifiers_header_from VARCHAR(20),
+                auth_results_dkim_domain VARCHAR(20),
+                auth_results_dkim_result VARCHAR(20),
+                auth_results_dkim_selector VARCHAR(20),
+                auth_results_spf_domain  VARCHAR(20),
+                auth_results_spf_result VARCHAR(20),
+                FOREIGN KEY (metadata_id)
+                     REFERENCES metadata (metadata_id) 
+          ) """,
+          "DROP TABLE reports"),
 ]
-
-
