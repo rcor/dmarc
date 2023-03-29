8da1bed00396c680a30050d6fe5ea0e75ef45cbf
Rodolfo Cordero <rcor.cr@gmail.com>
2023-03-29 18:19:17 -0300

    update

--- job/src/data.py
+++ job/src/data.py
@@ -1,20 +1,80 @@
 from configparser import ConfigParser
 import psycopg2
+import os
 
-def config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
+def config():
+    try:
+        user=os.environ['USER']
+        password=os.environ['PASSWORD']
+        database=os.environ['DATABASE']
+        host=os.environ['HOST']
+        connection = psycopg2.connect(database=database,
+                                user=user,
+                                password=password,
+                                host=host,
+                                port=5432)
+        return connection
+    except(Exception) as error:
+        print(error)
+    
 
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+def save_report(report):
+    try:
+        connection=config()
+        cursor = connection.cursor()
+        query="INSERT INTO report (report_id, org_name, file_name, date_begin, date_end) VALUES('{report_id}', '{org_name}', '{file_name}', '{date_begin}', '{date_end}');".format(
+            report_id=report.report_id,
+            org_name=report.org_name
+            file_name=report.file_name
+            date_begin=report.date_begin
+            date_end=report.date_end
+        )
+        records = cursor.execute()
+    except(Exception) as error:
+        print(error)
+    finally:
+        cursor.close()
+        connection.close()
 
-    return db
+def exist_report(report_id):
+    try:
+        connection=config()
+        cursor = connection.cursor()
+        query="select report_id from report where report_id={report_id}".format(report_id=report_id)
+        cursor.execute(query)
+        records=cursor.fetchall()
+        return True if records.count() > 0 else False
+    except(Exception) as error:
+        print(error)
+    finally:
+        cursor.close()
+        connection.close()
 
+
+def save_policy(report):
+    try:
+        connection=config()
+        cursor = connection.cursor()
+        query="INSERT INTO report (report_id, org_name, file_name, date_begin, date_end) VALUES('{report_id}', '{org_name}', '{file_name}', '{date_begin}', '{date_end}');".format(report_id=report.report_id,
+            org_name=report.org_name
+            file_name=report.file_name
+            date_begin=report.date_begin
+            date_end=report.date_end
+        )
+        records = cursor.execute()
+    except(Exception) as error:
+        print(error)
+    finally:
+        cursor.close()
+        connection.close()
+
+def save_report_info(report):
+    try:
+        connection=config()
+        cursor = connection.cursor()
+        records = cursor.execute()
+    except(Exception) as error:
+        print(error)
+    finally:
+        cursor.close()
+        connection.close()
--- migration/src/migrations/0000001.create_tables.py
+++ migration/src/migrations/0000001.create_tables.py
@@ -3,10 +3,24 @@ import os
 __depends__ = {}
 
 steps = [
-    step("CREATE TABLE metadata (metadata_id VARCHAR(20), org_name VARCHAR(20), file_name VARCHAR(20), date_begin timestamp, date_end timestamp, PRIMARY KEY (metadata_id))",
-         "DROP TABLE metadata"),
-    step("""CREATE TABLE reports (
-                report_id serial,metadata_id VARCHAR(20) NOT NULL,
+     step("CREATE TABLE report (report_id VARCHAR(20), org_name VARCHAR(20), file_name VARCHAR(20), date_begin timestamp, date_end timestamp, PRIMARY KEY (report_id))",
+         "DROP TABLE report"),
+     step("""CREATE TABLE POLICY(
+               policy_id serial,
+               report_id VARCHAR(20) NOT NULL,
+               domain VARCHAR(20),
+               adkim VARCHAR(20),
+               aspf VARCHAR(20),
+               p VARCHAR(20),
+               sp VARCHAR(20),
+               pct VARCHAR(20),
+               FOREIGN KEY (report_id)
+                     REFERENCES report (report_id) 
+           )
+          """,
+          "DROP TABLE POLICY;"),
+     step("""CREATE TABLE reports_info (
+                reports_info_id serial,report_id VARCHAR(20) NOT NULL,
                 source_ip VARCHAR(20), 
                 _count NUMERIC, 
                 policy_evaluated_disposition VARCHAR(20),
@@ -18,8 +32,8 @@ steps = [
                 auth_results_dkim_selector VARCHAR(20),
                 auth_results_spf_domain  VARCHAR(20),
                 auth_results_spf_result VARCHAR(20),
-                FOREIGN KEY (metadata_id)
-                     REFERENCES metadata (metadata_id) 
+                FOREIGN KEY (report_id)
+                     REFERENCES report (report_id) 
           ) """,
           "DROP TABLE reports"),
 ]
